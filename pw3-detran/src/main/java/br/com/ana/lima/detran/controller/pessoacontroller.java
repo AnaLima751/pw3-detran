22e4d96edfba78361ad3d718c5ace8256f6fdd63
Ana-Kaleb <Ana-Kaleb@test.com>
2022-05-04 14:29:29 -0300

    criacao controller e repository pessoa

---
+++ pw3-detran/src/main/java/br/com/ana/lima/detran/controller/pessoacontroller.java
@@ -0,0 +1,5 @@
+package br.com.ana.lima.detran.controller;
+
+public class pessoacontroller {
+
+}
---
+++ pw3-detran/src/main/java/br/com/ana/lima/detran/repository/pessoarepository.java
@@ -0,0 +1,5 @@
+package br.com.ana.lima.detran.repository;
+
+public interface pessoarepository {
+
+}
